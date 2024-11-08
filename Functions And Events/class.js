495b5a759802cec385aef22d1034341a9e5bd063
Someshwari16 <ravadar51@gmail.com>
2024-11-08 21:43:58 +0530

    Add files via upload

---
+++ Functions And Events/class.js
@@ -0,0 +1,12 @@
+class emp {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+
+    }
+}
+const emp1 = new emp("ravi", "25 years");
+console.log(emp1.name);
+console.log(emp1.age);
+
+
---
+++ Functions And Events/class2.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  }
+  
+  const person1 = new Person("ravi", 30);
+  console.log(person1.name); 
+  console.log(person1.age); 
+  
+  const person2 = new Person("ramu", 25);
+  console.log(person2.name); 
+  console.log(person2.age);
